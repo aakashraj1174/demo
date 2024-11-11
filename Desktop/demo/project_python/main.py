211204857883d4b0e89bf543bdc6019d430be40a
aakashraj1174 <aakashrajdhami174gmail.com>
2024-11-11 14:25:52 +0545

    human class

--- Desktop/demo/project_python/main.py
+++ Desktop/demo/project_python/main.py
@@ -1,3 +1,22 @@
-name = "Aakas"
+#name = "Aakas"
+#print(name)
 
-print(name)
+
+my_arr = [1, 2, "a", "names"]
+# add "ram"
+
+class Human:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def give_name(self):
+        print(f"Name: {self.name} and age: {self.age}.")
+    
+    def give_name(self):
+        print(f"name: {self.name}")
+
+
+new_human = Human(name="Aakas", age=12)
+print(new_human.name)
+new_human.give_name()
