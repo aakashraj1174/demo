57ade2dbad384d6fe3210ce1804d584e80831db3
aakashraj1174 <aakashrajdhami174gmail.com>
2024-11-11 14:40:42 +0545

    requirements added

--- Desktop/demo/project_python/main.py
+++ Desktop/demo/project_python/main.py
@@ -1,9 +1,10 @@
-#name = "Aakas"
-#print(name)
+# name = "Aakas"
+# print(name)
 
 
 my_arr = [1, 2, "a", "names"]
 # add "ram"
+
 
 class Human:
     def __init__(self, name, age):
@@ -12,7 +13,7 @@ class Human:
 
     def give_name(self):
         print(f"Name: {self.name} and age: {self.age}.")
-    
+
     def give_name(self):
         print(f"name: {self.name}")
 
